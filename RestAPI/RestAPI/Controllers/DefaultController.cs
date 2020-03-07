958ca1d10cb7f97e5cff9a298c699b5e6906ce6c
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-03-07 14:21:54 +0600

    Post method implemented and all versions deleted

---
+++ RestAPI/RestAPI/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromBody] PostRecieve postRecieve )
+        {
+            var name = postRecieve.MyName;
+            var age = postRecieve.MyAge;
+            var city = postRecieve.MyCity;
+
+            return name + "/" + age + "/" + city;
+        }
+
+    }
+}
---
+++ RestAPI/RestAPI/Models/PostRecieve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class PostRecieve
+    {
+        public string MyName { set; get; }
+        public string MyCity { set; get; }
+        public string MyAge { set; get; }
+
+    }
+}
